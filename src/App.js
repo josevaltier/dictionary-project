b838ec7784b0ac2e7df6d53e4e6b3fb35af46f78
josevaltier <84319890+josevaltier@users.noreply.github.com>
2021-09-15 13:55:57 +0200

    Setup + deleted unnecessary files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App container">
+      <header className="App-header">Hello</header>
+    </div>
+  );
+}
+
+export default App;
