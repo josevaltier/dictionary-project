6ca0b7b7dbbaca8589198f3a8cdcec408088da38
josevaltier <84319890+josevaltier@users.noreply.github.com>
2021-09-17 16:06:52 +0200

    Integrated Dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
   function searchWord(event) {
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     event.preventDefault();
-    alert(`Searching for "${keyword}" definition`);
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleResponse(response) {
+    console.log(response);
   }
 
   function handleKeywordChange(event) {
