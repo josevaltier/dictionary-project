97a739906a43746dffc27bf74eb2e13784770b6c
josevaltier <84319890+josevaltier@users.noreply.github.com>
2021-09-18 17:35:37 +0200

    Rendered the meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results.js";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
+  let [results, setResults] = useState(null);
+
   function searchWord(event) {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     event.preventDefault();
-
     axios.get(apiUrl).then(handleResponse);
   }
 
   function handleResponse(response) {
-    console.log(response);
+    setResults(response.data[0]);
   }
 
   function handleKeywordChange(event) {
@@ -24,10 +26,11 @@ export default function Dictionary() {
         <input
           type="search"
           placeholder="Search for..."
-          autofocus="on"
+          autoFocus="on"
           onChange={handleKeywordChange}
         />
       </form>
+      <Results results={results} />
     </div>
   );
 }
